3bc2532935ed2976869d94609c13006e3d4fec24
Helena Brito Dias <helenabdias95@gmail.com>
2021-07-29 22:06:12 -0300

    API class

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 
 import 'package:time_tracker_app/domain/domain.dart';
+import 'package:time_tracker_app/services/services.dart';
 
 abstract class Database {
   Future<void> createJob(Job job);
@@ -14,7 +15,7 @@ class FirestoreDatabase implements Database {
   }) : assert(uid != null);
 
   Future<void> createJob(Job job) async {
-    final path = '/users/$uid/jobs/job_abc';
+    final path = APIPath.job(uid, 'job_abc');
     final documentReference = FirebaseFirestore.instance.doc(path);
     await documentReference.set(job.toMap());
   }
--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,3 +1,4 @@
 export 'auth.dart';
 export 'auth_provider.dart';
 export 'database.dart';
+export 'api_path.dart';
