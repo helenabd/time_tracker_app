1528a26eccd2dbe337802cabcc6daef8f942612a
Helena Brito Dias <helenabdias95@gmail.com>
2021-08-03 13:33:13 -0300

    list items builder

--- lib/app/home/jobs/jobs.dart
+++ lib/app/home/jobs/jobs.dart
@@ -2,3 +2,4 @@ export 'edit_job_page.dart';
 export 'jobs_page.dart';
 export 'job_list_tile.dart';
 export 'empty_content.dart';
+export 'list_items_builder.dart';
--- lib/app/home/jobs/jobs_page.dart
+++ lib/app/home/jobs/jobs_page.dart
@@ -60,30 +60,16 @@ class JobsPage extends StatelessWidget {
     return StreamBuilder<List<Job>>(
       stream: database.jobsStream(),
       builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          final jobs = snapshot.data;
-          if (jobs.isNotEmpty) {
-            final children = jobs
-                .map((job) => JobListTile(
-                      job: job,
-                      onTap: () => EditJobPage.show(
-                        context,
-                        job: job,
-                      ),
-                    ))
-                .toList();
-            return ListView(
-              children: children,
-            );
-          }
-          return EmptyContent();
-        }
-        if (snapshot.hasError) {
-          return Center(
-            child: Text('Some error occurred'),
-          );
-        }
-        return Center(child: CircularProgressIndicator());
+        return ListItemsBuilder<Job>(
+          snapshot: snapshot,
+          itemBuilder: (context, job) => JobListTile(
+            job: job,
+            onTap: () => EditJobPage.show(
+              context,
+              job: job,
+            ),
+          ),
+        );
       },
     );
   }
