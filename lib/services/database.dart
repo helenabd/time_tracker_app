c7b2b3cd50a624ff1a9852f016c74b96080b1c4c
Helena Brito Dias <helenabdias95@gmail.com>
2021-08-03 10:35:01 -0300

    Completing the code for editing jobs

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -4,7 +4,7 @@ import 'package:time_tracker_app/domain/domain.dart';
 import 'package:time_tracker_app/services/services.dart';
 
 abstract class Database {
-  Future<void> createJob(Job job);
+  Future<void> setJob(Job job);
   Stream<List<Job>> jobsStream();
 }
 
@@ -18,8 +18,8 @@ class FirestoreDatabase implements Database {
   final String uid;
   final _service = FirestoreServices.instance;
 
-  Future<void> createJob(Job job) => _service.setData(
-        path: APIPath.job(uid, documentIdFromCurrentDate()),
+  Future<void> setJob(Job job) => _service.setData(
+        path: APIPath.job(uid, job.id),
         data: job.toMap(),
       );
 
